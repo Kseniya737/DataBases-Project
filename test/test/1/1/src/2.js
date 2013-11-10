1ecf7829a781020b06b0ef332b51c8faa5d9dba9
kseniya737 <kseniya737@mail.ru>
2013-11-10 15:03:39 +0400

    Первый этап

---
+++ test/test/1/1/src/2.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @author ксюша
+ * @name _
+ */
+
+function textFieldActionPerformed(evt) {//GEN-FIRST:event_textFieldActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_textFieldActionPerformed
+
